3c98fb5aa20f627361fb6683e6abf9100bde5137
OtarMurmanishvili <94814294+Rektovski@users.noreply.github.com>
2023-06-23 16:52:44 +0400

    Update Main.js

--- src/main/components/mainComponents/Main.js
+++ src/main/components/mainComponents/Main.js
@@ -77,7 +77,7 @@ export default function Main() {
     };
 
     const logout = () => {
-        localStorage.clear();
+        localStorage.removeItem('user');
         window.location.reload();
     }
 
